315aa29562d32877c304ae68395bc931fd1ee070
G0hu <hpo897@gmail.com>
2017-03-14 15:14:08 +0100

    added all libs

--- Cropper/ViewController.swift
+++ Cropper/ViewController.swift
@@ -8,26 +8,47 @@
 
 import UIKit
 import EasyImagy
+import TOCropViewController
 import ALCameraViewController
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, TOCropViewControllerDelegate {
 
+    var b: UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.white
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        let cameraViewController = CameraViewController(croppingEnabled: false) { [weak self] image, asset in
-            self?.dismiss(animated: true, completion: nil)
-        }
         
-        present(cameraViewController, animated: true, completion: nil)
+        b = UIButton(frame: self.view.frame)
+        b?.backgroundColor = UIColor.clear
+        b?.addTarget(self, action: #selector(showCameraRoll), for: .touchUpInside)
+        
+        view.addSubview(b!)
+        view.backgroundColor = UIColor.white
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc private func showCameraRoll() {
+        let cameraViewController = CameraViewController(croppingEnabled: false) { [weak self] image, asset in
+            self?.dismiss(animated: true, completion: { self?.presentCropViewController(image: image!) })
+        }
+        
+        present(cameraViewController, animated: true, completion: nil)
+    }
+    
+    private func presentCropViewController(image: UIImage) {
+        let cropViewController: TOCropViewController = TOCropViewController(image: image)
+        cropViewController.delegate = self
+        
+        self.present(cropViewController, animated: true, completion: nil)
+    }
+    
+    func cropViewController(_ cropViewController: TOCropViewController, didCropToImage image: UIImage, rect cropRect: CGRect, angle: Int) {
+        print("image cropped")
+        self.dismiss(animated: true, completion: nil)
     }
 }
 
