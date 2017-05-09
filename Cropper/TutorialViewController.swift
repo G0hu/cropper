ee3c999921e6bad347e0f5c6dc725c6fbab57754
G0hu <hpo897@gmail.com>
2017-05-10 01:19:33 +0200

    nearly finished walkthrough

--- Cropper/TutorialViewController.swift
+++ Cropper/TutorialViewController.swift
@@ -11,45 +11,50 @@ import Onboard
 import Foundation
 
 func generateWalkThrough(rect: CGRect) -> OnboardingViewController {
-    let page1 = CustomOBContentViewController(title: "", body: "Take a picture or choose\none for your library",
-                                              image: #imageLiteral(resourceName: "icon-p1"), buttonText: "", action: nil)
-    let page2 = CustomOBContentViewController(title: "", body: "Choose in how many pieces\nyou want to split\nthe picture",
-                                                image: #imageLiteral(resourceName: "icon-p2"), buttonText: "", action: nil)
-    let page3 = CustomOBContentViewController(title: "", body: "Adjust the cropping\nwindow... And you're done",
-                                                image: #imageLiteral(resourceName: "icon-p3"), buttonText: "", action: nil)
+    
+    var images: Array<UIImage> = []
+    let background: UIImage = #imageLiteral(resourceName: "trees")
+    let newBackground = resizeImage(source: background, width: rect.width * 3)
+
+    
+    for i in 0...2 {
+        let r = CGRect(x: rect.width * CGFloat(i), y: 0.0, width: rect.width, height: newBackground.size.height)
+        let img: UIImage = newBackground.croppedImage(withFrame: r, angle: 0, circularClip: false)
+        images.append(img)
+    }
+    
+    let page1 = CustomOBContentViewController(body: "Take a picture or choose\none for your library",
+                                              image: #imageLiteral(resourceName: "icon-p1"), background: images[0])
+    let page2 = CustomOBContentViewController(body: "Choose in how many pieces\nyou want to split\nthe picture",
+                                              image: #imageLiteral(resourceName: "icon-p2"), background: images[1])
+    let page3 = CustomOBContentViewController(body: "Adjust the cropping\nwindow... And you're done",
+                                              image: #imageLiteral(resourceName: "icon-p3"), background: images[2])
     
     page1.topPadding = 135
-    page1.frame = CGSize(width: 120, height: 91)
-    
+    page1.icon_frame = CGSize(width: 120, height: 91)
     page2.topPadding = 135
-    page2.frame = CGSize(width: 215, height: 75)
-    
+    page2.icon_frame = CGSize(width: 215, height: 75)
     page3.topPadding = 135
-    page3.frame = CGSize(width: 110, height: 110)
+    page3.icon_frame = CGSize(width: 110, height: 110)
     
-    let obvc = CustomOBViewController(backgroundImage: imageWithalpha(img: #imageLiteral(resourceName: "trees"), alpha: 0.0),
-                                      contents: [page1, page2, page3])
+    let obvc = CustomOBViewController(backgroundImage: UIImage(), contents: [page1, page2, page3])
     obvc?.shouldMaskBackground = false
     obvc?.allowSkipping = true
     return obvc!
 }
 
-func imageWithalpha(img: UIImage, alpha: CGFloat) -> UIImage {
-    UIGraphicsBeginImageContextWithOptions(img.size, false, 0.0)
+func resizeImage(source: UIImage, width: CGFloat) -> UIImage {
+    let oldWidth: CGFloat = source.size.width
+    let scaleFactor: CGFloat = width / oldWidth
+    let newHeight: CGFloat = source.size.height * scaleFactor
     
-    let ctx: CGContext  = UIGraphicsGetCurrentContext()!
-    let area: CGRect = CGRect(x: 0.0, y: 0.0, width: img.size.width, height: img.size.height)
+    let newWidth: CGFloat = oldWidth * scaleFactor
+    UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight));
+    source.draw(in: CGRect(x: 0.0, y: 0.0, width: newWidth, height: newHeight))
     
-    ctx.scaleBy(x: 1, y: -1)
-    ctx.translateBy(x: 0, y: -area.size.height)
-    ctx.setBlendMode(.multiply)
-    
-    ctx.setAlpha(alpha)
-    ctx.draw(img.cgImage!, in: area)
     let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-    
     UIGraphicsEndImageContext()
-    return newImage;
+    return newImage
 }
 
 class CustomOBViewController: OnboardingViewController {
@@ -70,13 +75,49 @@ class CustomOBViewController: OnboardingViewController {
 
 class CustomOBContentViewController: OnboardingContentViewController {
     
-    var frame: CGSize = CGSize() {
+    var backgrnd: UIImageView?
+    
+    var icon_frame: CGSize = CGSize() {
         didSet {
-            self.iconWidth = frame.width
-            self.iconHeight = frame.height
+            self.iconWidth = icon_frame.width
+            self.iconHeight = icon_frame.height
         }
     }
 
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    override init(title: String?, body: String?, image: UIImage?, buttonText: String?, action: (() -> Void)? = nil) {
+        super.init(title: title, body: body, image: image, buttonText: buttonText, action: action)
+    }
+    
+    override init(title: String?, body: String?, image: UIImage?, buttonText: String?, actionBlock: action_callback? = nil) {
+        super.init(title: title, body: body, image: image, buttonText: buttonText, actionBlock: actionBlock)
+    }
+    
+    override init(title: String?, body: String?, videoURL: URL?, buttonText: String?, action: (() -> Void)? = nil) {
+        super.init(title: title, body: body, videoURL: videoURL, buttonText: buttonText, action: nil)
+    }
+    
+    override init(title: String?, body: String?, image: UIImage?, videoURL: URL?, buttonText: String?,
+                  actionBlock: action_callback? = nil) {
+        super.init(title: title, body: body, image: image, videoURL: videoURL,
+                   buttonText: buttonText, actionBlock: actionBlock)
+    }
+    
+    init(body: String?, image: UIImage?, background: UIImage?) {
+        super.init(title: "", body: body, image: image, buttonText: "", action: nil)
+        backgrnd = UIImageView(image: background)
+        print(background?.size, backgrnd?.frame)
+        
+        backgrnd?.alpha = 0.25
+    }
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
@@ -84,19 +125,19 @@ class CustomOBContentViewController: OnboardingContentViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setNeedsStatusBarAppearanceUpdate()
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
         
         bodyLabel.numberOfLines = 2
         bodyLabel.translatesAutoresizingMaskIntoConstraints = false
         bodyLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         bodyLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150).isActive = true
+        
+        view.insertSubview(backgrnd!, at: 0)
+        print(backgrnd?.frame)
+        backgrnd?.translatesAutoresizingMaskIntoConstraints = false
+        backgrnd?.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        backgrnd?.widthAnchor.constraint(equalToConstant: ((backgrnd?.image?.size.width)! * (backgrnd?.image?.scale)!))
+            .isActive = true
+        backgrnd?.heightAnchor.constraint(equalToConstant: ((backgrnd?.image?.size.height)! * (backgrnd?.image?.scale)!))
+            .isActive = true
     }
 }
-
-
-
-
-
